89e16b5266ce10febeeef2ad902ca1fd7417cb4f
vokhaiminh0811 <vokhaiminh0811@gmail.com>
2021-04-25 18:30:44 +0700

    Include JS defer

--- script/textValidation.js
+++ script/textValidation.js
@@ -1,3 +1,12 @@
 //TODO: Minh
 
 const questions = [4, 6];
+
+
+function validateForm() {
+    var x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out");
+      return false;
+    }
+  }
