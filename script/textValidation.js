e9967667c9ca54d10e3f6eac6ad05503a8550c27
vokhaiminh0811 <vokhaiminh0811@gmail.com>
2021-05-02 05:13:47 +0700

    Add validation for name and phone in contact.html

--- script/textValidation.js
+++ script/textValidation.js
@@ -5,50 +5,57 @@ const questions = [4, 6];
 const userName = document.getElementById("name");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
-console.log('phone :>> ', phone);
+console.log("phone :>> ", phone);
 const submitButton = document.getElementById("submitButton");
-console.log('form :>> ', submitButton);
 const errorElement = document.getElementById("error");
 
-let messages = [];
-//name
-submitButton.addEventListener("mouseover", () => {
-  if (userName.value === "" || userName.value == null) {
-    messages.push("Name is required");
-  }
+submitButton.addEventListener("click", () => {
+  const error = document.getElementById("notify");
+  // userName.setCustomValidity("");
+  // phone.setCustomValidity("");
+
+  // // const numberRegex=
+  // const checkIfNameHasNumber = () =>
+  //   Array.from(userName.value).forEach((item) => {
+  //     isNaN(parseInt(item))? true
+  //   });
+  // console.log(
+  //   "(userName.innerHTML :>> ",
+  //   Array.from(userName.value).forEach((item) =>
+  //     typeof parseInt(item) === Number ? true : false
+  //   )
+  // );
+  // console.log(
+  //   "    Array.from(userName).map((item) => (isNaN(item) ?) :>> ",
+  //   checkIfNameHasNumber
+  // );
+  userName.patternMismatch
+    ? userName.setCustomValidity(
+        "Name must exceeds 3 characters and contain letters only!"
+      )
+    : {};
+
+  !phone.patternMismatch
+    ? phone.setCustomValidity("Phone number should contains number only!")
+    : {};
 });
+//  else if (!userName.patternMismatch) {
+//   userName.setCustomValidity("Name must exceed 3 characters!");
 
-if (userName.value.length <= 3) {
-  messages.push("Password must be longer than 3 characters");
-}
+// if (messages.length > 0) {
+//   e.preventDefault();
+//   alert(message);
+// }
 
-if (messages.length > 0) {
-  e.preventDefault();
-  alert(message)
-}
-
-//email
-function ValidateEmail(mail) {
-  if (
-    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
-      myForm.email.value
-    )
-  ) {
-    return true;
-  }
-  alert("You have entered an invalid email address!");
-  return false;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
+// email
+// function ValidateEmail(mail) {
+//   if (
+//     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+//       myForm.email.value
+//     )
+//   ) {
+//     return true;
+//   }
+//   alert("You have entered an invalid email address!");
+//   return false;
+// }
