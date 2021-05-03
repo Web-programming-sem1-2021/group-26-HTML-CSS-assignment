eed79e9b204d4d81e2bedc1ca2ff5db6c80b42f1
vokhaiminh0811 <vokhaiminh0811@gmail.com>
2021-05-03 18:54:56 +0700

    Validate form checkboxes

--- script/contactTextValidation.js
+++ script/contactTextValidation.js
@@ -7,13 +7,35 @@ const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const contactSubmitButton = document.getElementById("contactSubmitButton");
 const errorElement = document.getElementById("error");
+const form = document.querySelector("#sectionForm");
+const scheduleCheckboxes = form.querySelectorAll("input[type=checkbox]");
+const firstScheduleCheckbox = document.getElementById("day1");
+
+const validateCheckboxes = () => {
+  let checkNumber = 0;
+  Array.from(scheduleCheckboxes).forEach((checkbox) => {
+    checkbox.checked ? (checkNumber += 1) : checkNumber === checkNumber;
+  });
+  return checkNumber >= 1;
+};
+
+console.log("scheduleCheckboxes :>> ", scheduleCheckboxes);
 
 //Contact
-contactSubmitButton.addEventListener("click", () => {
+contactSubmitButton.addEventListener("click", (e) => {
   const error = document.getElementById("notify");
   const namePattern = /[A-Za-z]{3,}/;
   const phoneNumberPattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,5}$/im;
   const emailPattern = /^([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-])+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  !validateCheckboxes()
+    ? (firstScheduleCheckbox.setCustomValidity("Check atleast 1 checkbox!"),
+      console.log("hahaahaa :>> ", validateCheckboxes()),
+      Array.from(scheduleCheckboxes).forEach(
+        (checkbox) => (checkbox.style.boxShadow = "0 0 10px red")
+      ),
+      e.preventDefault())
+    : null;
 
   // userName.setCustomValidity("");
   // phone.setCustomValidity("");
@@ -38,17 +60,18 @@ contactSubmitButton.addEventListener("click", () => {
   console.log("object :>> ", namePattern.test(userName.value));
   namePattern.test(userName.value)
     ? userName.setCustomValidity("")
-    : userName.setCustomValidity(
+    : (userName.setCustomValidity(
         "Name must exceeds 3 characters and contain letters only!"
-      );
-
+      ),
+      (userName.style.boxShadow = "0 0 10px red"));
   //E-mail
   console.log(emailPattern.test(email.value));
   emailPattern.test(email.value)
     ? email.setCustomValidity("")
-    : email.setCustomValidity(
+    : (email.setCustomValidity(
         "Your email must be in a correct form (abc#def@mail.com)"
-      );
+      ),
+      (email.style.boxShadow = "0 0 10px red"));
   //Phone number
   console.log(
     " phoneNumberPattern.test(phone.value) :>> ",
@@ -56,20 +79,8 @@ contactSubmitButton.addEventListener("click", () => {
   );
   phoneNumberPattern.test(phone.value)
     ? phone.setCustomValidity("")
-    : phone.setCustomValidity("Phone number should contains number only!");
-
-  console.log(namePattern.test(firstName.value));
-  namePattern.test(firstName.value)
-    ? firstName.setCustomValidity("")
-    : firstName.setCustomValidity(
-        "Your first name must exceeds 3 characters and contain letters only!"
-      );
-
-  namePattern.test(lastName.value)
-    ? lastName.setCustomValidity("")
-    : lastName.setCustomValidity(
-        "Your last name must exceeds 3 characters and contain letters only!"
-      );
+    : (phone.setCustomValidity("Phone number should contains number only!"),
+      (phone.style.boxShadow = "0 0 10px red"));
 });
 
 //  else if (!userName.patternMismatch) {
--- script/registerTextValidation.js
+++ script/registerTextValidation.js
@@ -7,20 +7,25 @@ const lastName = document.getElementById("last-name");
 registerSubmitButton.addEventListener("click", (e) => {
   const namePattern = /[A-Za-z]{3,}/;
   //   const phoneNumberPattern = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{3,5}$/im;
-  const emailPattern = /([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-])+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)/;
-
+  const emailPattern = /^([a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-])+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   //E-mail
 
+  console.log(
+    "emailPattern.test(email.value)>> ",
+    emailPattern.test(email.value)
+  );
+
   emailPattern.test(email.value)
-    ? email.setCustomValidity("")
+    ? (email.setCustomValidity("lkjidshfkljashjflkj"),
+      email.reportValidity(),
+      e.preventDefault())
     : (email.setCustomValidity(
         "Your email must be in a correct form (abc#def@mail.com)"
       ),
-      (email.style.boxShadow = "0 0 10px red"),
-      email.reportValidity());
-
+      email.reportValidity(),
+      e.preventDefault());
   namePattern.test(firstName.value)
-    ? firstName.setCustomValidity("")
+    ? (firstName.setCustomValidity(""), e.preventDefault())
     : (firstName.setCustomValidity(
         "Your first name must exceeds 3 characters and contain letters only!"
       ),
@@ -29,7 +34,7 @@ registerSubmitButton.addEventListener("click", (e) => {
       e.preventDefault());
 
   namePattern.test(lastName.value)
-    ? lastName.setCustomValidity("")
+    ? (lastName.setCustomValidity(""), e.preventDefault())
     : (lastName.setCustomValidity(
         "Your last name must exceeds 3 characters and contain letters only!"
       ),
