5b3c0fe1f27a57cdaece93e7ca488fe9efd808cb
giangnguyen <giang.nguyen@skedulo.com>
2021-05-03 15:53:01 +0700

    Draft functions for controlling local storage

--- script/login.js
+++ script/login.js
@@ -1,3 +1,18 @@
 //TODO: Giang
 
 const questions = [5];
+
+let accountNavBarLink = document
+  .getElementById("navBarAccount")
+  .getAttribute("href");
+const loginSubmitButton = document.getElementById("login-submit");
+const userName = document.getElementById("usrname")
+const password = document.getElementById('passcode')
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Handler when the DOM is fully loaded
+  console.log(`object`, accountNavBarLink.slice(accountNavBarLink.length - 10));
+});
+
+
+loginSubmitButton.addEventListener('click')
